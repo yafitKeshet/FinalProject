e4eabc1590193eba5a312612b42ee51ffb46490b
pazbechor <pbechor@paloaltonetworks.com>
2023-05-06 18:11:37 +0300

    Adding send email in signup process

--- BE/lib/rest/signup.py
+++ BE/lib/rest/signup.py
@@ -7,49 +7,62 @@ from BE.lib.utils.auth.decode_token import get_current_active_user
 from BE.lib.utils.auth.generate_access_token import login_for_access_token
 from BE.lib.utils.db.models.user import User
 from BE.lib.utils.db.user_db import get_db_session, UserDBSession
+from BE.lib.utils.mail_handler.mail_sender import EmailSender
 from BE.lib.utils.rest_models import Login, UserProfileIn, SignUpUserProfile, UserProfileOut, OnBoardingUserProfile, \
-    UserLogin
+    UserLogin, SignupSecondStep
 
 router = APIRouter()
 
 
-# ToDo: Will be used later
-# # Register new user
-# # 200 - Success
-# # 409 - User already exist
-# @router.post(
-#     "/signUp/firstStep",
-#     name="Sign up new profile. Send code to user email (in order to be submitted in second step",
-#     description="This endpoint should happen only after validate DO NOT already exists",
-#     status_code=status.HTTP_200_OK,
-#     response_model=None
-# )
-# def sign_up_new_profile_first_step(
-#         mta_user_email: str
-# ):
-#     pass
-#
-#
-# # 400 - temp code is not correct
-# # 404 - User email does not exists in this "step" (Creation)
-# @router.post(
-#     "/signUp/secondStep",
-#     name="Sign up new profile",
-#     description="This step happens after First step - The user should enter a temp code (got by email). By doing this"
-#                 "we ensure that the email is really belong to the user",
-#     status_code=status.HTTP_200_OK,
-#     response_model=Login
-# )
-# def sign_up_new_profile_second_step(
-#         user_email: str,
-#         temp_code: str,
-# ):
-#     pass
+# Register new user
+# 200 - Success
+# 409 - User already exist
+@router.post(
+    "/signUp/firstStep",
+    name="Sign up new profile. Send code to user email (in order to be submitted in second step",
+    description="This endpoint should happen only after validate DO NOT already exists",
+    status_code=status.HTTP_200_OK,
+    response_model=bool
+)
+def sign_up_new_profile_first_step(
+        user_email: str,
+        db: UserDBSession = Depends(get_db_session),
+):
+    EmailSender(db).send_mail_to_user(user_email)
+    return True
 
 
-# 201 - Created
-# 409 - User already exists
-# 400 - missing details
+
+# 400 - temp code is not correct
+# 404 - User email does not exist in this "step" (Creation)
+@router.post(
+    "/signUp/secondStep",
+    name="Sign up new profile",
+    description="This step happens after First step - The user should enter a temp code (got by email). By doing this"
+                "we ensure that the email is really belong to the user"
+                "Return value repeesent if the mail belongs to the user in out point of view",
+    status_code=status.HTTP_200_OK,
+    response_model=bool
+)
+def sign_up_new_profile_second_step(
+        body: SignupSecondStep,
+        db: UserDBSession = Depends(get_db_session),
+):
+    # First Assert that temp password is correct
+    existing_user_temp_password = db.get_user_temp_password_entry(body.user_email)
+    if not existing_user_temp_password:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"The user {body.user_email} is not in this step")
+
+    if existing_user_temp_password.temp_password != body.temp_password:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"The temp password is incorrect for user {body.user_email}")
+
+    # Delete the temp password entry from DB
+    db.delete(existing_user_temp_password)
+    # Committing changes
+    db.commit()
+    return True
+
+
 @router.post(
     "/signUp",
     name="Sign up new profile",
