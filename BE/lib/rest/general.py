2ff7da35dce80b158098298b1968443050fe58b1
pazbechor <pbechor@paloaltonetworks.com>
2023-05-06 18:42:39 +0300

    .

--- BE/lib/rest/general.py
+++ BE/lib/rest/general.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status
 
+from BE.lib.utils.enums import JobTime, Experience, Faculty, Year, Rating, EnumsResponse
+
 router = APIRouter()
 
 
@@ -7,8 +9,14 @@ router = APIRouter()
     "/enums",
     name="Get all Enums as JSON for Faculty: list of faculties",
     status_code=status.HTTP_200_OK,
-    response_model=dict,
+    response_model=EnumsResponse,
     description="Getting all Enums. Purpose: FrontEnd will be able to convert the Backend values to it's desired values"
 )
 def get_enums():
-    pass
+    return {
+        "JobTime": [item for item in JobTime],
+        "Experience": [item for item in Experience],
+        "Faculty": [item for item in Faculty],
+        "Year": [item for item in Year],
+        "Rating": [item for item in Rating]
+    }
