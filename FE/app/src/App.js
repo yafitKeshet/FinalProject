5bdd5fb31354a88b6c7ef9dc2f0a3ba86089a66b
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-05-10 16:20:26 +0300

    Update App.js

--- FE/app/src/App.js
+++ FE/app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Menu from "./components/header/Menu";
 
-function App() {
+const INITIAL_MENU = [
+  {
+    onclick: {},
+    data: "מידע כללי",
+  },
+  {
+    onclick: {},
+    data: "פקולטות",
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu items={INITIAL_MENU} />
     </div>
   );
-}
+};
 
 export default App;
