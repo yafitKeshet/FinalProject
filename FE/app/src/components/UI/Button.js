f83f1d8ad9cc0bfc053323a88c57cd2bd47a74d1
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-05-10 18:57:23 +0300

    Update Button.js

--- FE/app/src/components/UI/Button.js
+++ FE/app/src/components/UI/Button.js
@@ -4,7 +4,7 @@ import classes from "./Button.module.css";
 const Button = (props) => {
   return (
     <button
-      className={classes.button}
+      className={`${classes.button} ${props.className}`}
       type={props.type || "button"}
       onClick={props.onClick}
     >
