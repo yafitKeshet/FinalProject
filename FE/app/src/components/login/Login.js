a3bb13ca103309830f8488749bf4b488f548c7da
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-05-11 23:02:58 +0300

    update

--- FE/app/src/components/login/Login.js
+++ FE/app/src/components/login/Login.js
@@ -8,6 +8,14 @@ const Login = (props) => {
       <div className="title">
         <p>אתר הבוגרים, האקדמית תל אביב יפו</p>
       </div>
+      <div className="login-description">
+        <p>
+          קהילת בוגרי האקדמית תל אביב יפו.
+          <br />
+        </p>
+        זה הזמן להצטרף לקהילת הבוגרות והבוגרים המשפיעה בישראל, בכדי שנוכל לעדכנך
+        במגוון אירועים, חדשות והזדמנויות לפיתוח קריירה
+      </div>
       <img
         className="login-img"
         src="https://www.mta.ac.il/he-il/PublishingImages/Lists/Plazma/AllItems/549A2139_RAW.jpg"
