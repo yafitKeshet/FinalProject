ceea1dddcaa62c3bb731fd365e80da86335a5dba
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-05-11 12:53:07 +0300

    Update LoginForm.js

--- FE/app/src/components/login/LoginForm.js
+++ FE/app/src/components/login/LoginForm.js
@@ -4,23 +4,49 @@ import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 const LoginForm = (props) => {
+  // Input Colors
+  const [passBorderColor, setPassBorderColor] = useState("#ccc");
+  const [passBackgroundColor, setPassBackgroundColor] = useState("");
+  const [confirmPassBorderColor, setConfirmPassBorderColor] = useState("#ccc");
+  const [confirmPassBackgroundColor, setConfirmPassBackgroundColor] =
+    useState("");
+
+  // Input fields
+  const [mailBorderColor, setMailBorderColor] = useState("#ccc");
+  const [mailBackgroundColor, setMailBackgroundColor] = useState("");
   const [enteredMail, setEnteredMail] = useState("");
   const [enteredPass, setEnteredPass] = useState("");
   const [enteredConfirmPass, setEnteredConfirmPass] = useState("");
 
+  // Input fields handlers
   const mailChangeHandler = (event) => {
+    let isValid =
+      event.target.value.endsWith("@mta.ac.il") &&
+      event.target.value.length > 10;
     setEnteredMail(event.target.value);
+    setMailBorderColor(isValid ? "green" : "red");
+    setMailBackgroundColor(isValid ? "rgb(117, 250, 113)" : "#f86262");
   };
   const passChangeHandler = (event) => {
+    let isValid = event.target.value.length > 0;
     setEnteredPass(event.target.value);
-  };
-  const confirmPassChangeHandler = (event) => {
-    setEnteredConfirmPass(event.target.value);
+    setPassBorderColor(isValid ? "green" : "red");
+    setPassBackgroundColor(isValid ? "rgb(117, 250, 113)" : "#f86262");
   };
 
+  const confirmPassChangeHandler = (event) => {
+    let isValid =
+      event.target.value.length > 0 && event.target.value === enteredPass;
+    setEnteredConfirmPass(event.target.value);
+    setConfirmPassBorderColor(isValid ? "green" : "red");
+    setConfirmPassBackgroundColor(isValid ? "rgb(117, 250, 113)" : "#f86262");
+  };
+
+  // Login handler
   const submitHandler = (event) => {
     event.preventDefault();
 
+    // TODO
     // const userData = {
     //   mail: enteredMail,
     //   pass: enteredPass,
@@ -30,9 +56,13 @@ const LoginForm = (props) => {
     setEnteredPass("");
     setEnteredConfirmPass("");
 
+    // TODO
     props.onLogIn(true);
     // props.onSaveUser(userData);
   };
+
+  // Register handler
+  // TODO
 
   return (
     <div className="login-page">
@@ -53,24 +83,38 @@ const LoginForm = (props) => {
                 placeholder="some@mta.ac.il"
                 pattern="[a-z0-9._%+-]+@mta.ac.il"
                 title="The email should be of the Academic Tel-Aviv Yafo."
+                style={{
+                  border: `2px solid ${mailBorderColor}`,
+                  backgroundColor: `${mailBackgroundColor}`,
+                }}
                 required
               ></input>
             </div>
             <div className="login-control">
               <label>סיסמא</label>
               <input
-                type="text"
+                type="password"
                 value={enteredPass}
                 onChange={passChangeHandler}
+                style={{
+                  border: `2px solid ${passBorderColor}`,
+                  backgroundColor: `${passBackgroundColor}`,
+                }}
                 required
               ></input>
             </div>
             <div className="login-control">
               <label>אימות סיסמא</label>
               <input
-                type="text"
+                type="password"
                 value={enteredConfirmPass}
                 onChange={confirmPassChangeHandler}
+                pattern={enteredPass.toString()}
+                style={{
+                  border: `2px solid ${confirmPassBorderColor}`,
+                  backgroundColor: `${confirmPassBackgroundColor}`,
+                }}
+                title="Password not match."
                 required
               ></input>
             </div>
@@ -80,12 +124,14 @@ const LoginForm = (props) => {
             <Button className="login-btn" type="submit">
               התחברות
             </Button>
+
+            {/* TODO */}
             <Button className="register-btn">
               <p>
                 אין לך משתמש?
                 <br />
                 לחץ להרשמה
-              </p>{" "}
+              </p>
             </Button>
           </div>
         </Card>
