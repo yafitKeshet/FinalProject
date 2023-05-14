ee06585b20b9f8c534e32aa1b4c8d438b326603d
pazbechor <pbechor@paloaltonetworks.com>
2023-05-14 13:41:26 +0300

    .

--- BE/main.py
+++ BE/main.py
@@ -34,16 +34,11 @@ def prepare_app():
 
 logging.basicConfig(level=logging.DEBUG)
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-]
 
 app = prepare_app()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
