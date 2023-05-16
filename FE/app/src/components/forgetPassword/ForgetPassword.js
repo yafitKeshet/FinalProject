0a8d4ebc4a0d65f144e44753a35cc4b90dac0f17
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-05-16 23:52:48 +0300

    step1

--- FE/app/src/components/forgetPassword/ForgetPassword.js
+++ FE/app/src/components/forgetPassword/ForgetPassword.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import Separator from "../UI/Separator";
@@ -6,28 +6,42 @@ import Separator from "../UI/Separator";
 import "./ForgetPassword.css";
 
 const ForgetPassword = (props) => {
+  const [steps, setSteps] = useState({
+    step1: true,
+    step2: false,
+    step3: false,
+    step4: false,
+    step5: false,
+  });
+
   return (
     <form className="forget-form">
       <div className="backdrop" onClick={props.onCancel} />
       <Card className="forget-card">
         <header className="forget-header">
-          <h2>איתור החשבון שלך</h2>
+          <h2> {steps.step1 && "איתור החשבון שלך"}</h2>
         </header>
         <Separator />
 
         <div className="forget-contents">
-          <label className="forget-content">
-            יש להזין את כתובת הדוא"ל כדי לחפש את החשבון שלך.
-          </label>
+          {steps.step1 && (
+            <label className="forget-content">
+              {steps.step1 &&
+                `	
+יש להזין את כתובת הדוא"ל או מספר הטלפון כדי לחפש את החשבון שלך.`}
+            </label>
+          )}
 
-          <input
-            className="forget-content"
-            type="email"
-            placeholder="some@mta.ac.il"
-            pattern="[a-zA-Z0-9._%+-]+@mta.ac.il"
-            title="The email should be of the Academic Tel-Aviv Yafo."
-            required
-          />
+          {steps.step1 && (
+            <input
+              className="forget-content"
+              type="email"
+              placeholder="some@mta.ac.il"
+              pattern="[a-zA-Z0-9._%+-]+@mta.ac.il"
+              title="The email should be of the Academic Tel-Aviv Yafo."
+              required
+            />
+          )}
         </div>
         <Separator />
         <footer className="forget-actions">
@@ -35,7 +49,7 @@ const ForgetPassword = (props) => {
             ביטול
           </Button>
           <Button className="forget-send" type="submit">
-            חיפוש
+            {steps.step1 && "חיפוש"}
           </Button>
         </footer>
       </Card>
