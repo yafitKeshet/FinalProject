fdeb40390f25d87298a50132f211c827f80f4e9f
Lonnie Berry <lonchbe@gmail.com>
2023-05-27 11:26:53 +0300

    update profile - save

--- BE/lib/rest/profile.py
+++ BE/lib/rest/profile.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status, Depends
+
+from fastapi import APIRouter, status, Depends, HTTPException
 from typing_extensions import Annotated
 from ..utils.db.models.user import User
 from ..utils.auth.decode_token import get_current_active_user
@@ -16,8 +17,7 @@ router = APIRouter()
     response_model=UserProfileOut
 )
 def get_profile(
-        user: Annotated[User, Depends(get_current_active_user)],
-        db: UserDBSession = Depends(get_db_session)
+        user: Annotated[User, Depends(get_current_active_user)]
 ):
     return UserProfileOut(**{**user.__dict__})
 
@@ -31,13 +31,19 @@ def get_profile(
     description="User can update any fields he wants (according to Scheme) -"
                 "After the request the user details should refresh (changing State / invoking a get request)",
     status_code=status.HTTP_200_OK,
-    response_model=UserProfileOut
+    response_model=bool
 )
 def update_profile(
         user: Annotated[User, Depends(get_current_active_user)],
-        user_profile: UpdateUserProfile
+        user_data: UpdateUserProfile,
+        db: UserDBSession = Depends(get_db_session)
 ):
-    pass
+    existing_course = db.get_user_query(user.user_email).first()
+    if existing_course is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Course was not found")
+    db.db.get_user_query(user.user_email).update({k: v for k, v in user_data.dict().items() if v is not None})
+    db.commit()
+    return True
 
 
 @router.get(
