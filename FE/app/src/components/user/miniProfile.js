4b5fcb086f041e40e97b3de3dd981d533ceb13d3
Yafit Mizrahi <94388603+yafitKeshet@users.noreply.github.com>
2023-06-25 23:23:52 +0300

    update

--- FE/app/src/components/user/miniProfile.js
+++ FE/app/src/components/user/miniProfile.js
@@ -8,43 +8,52 @@ import Birthday from "../UI/SVG/Birthday";
 import College from "../UI/SVG/College";
 import Year from "../UI/SVG/Year";
 import Job from "../UI/SVG/Job";
-import { Faculty, Year as CollegeYear } from "../enums/enums.ts";
+import { getFaculty, getYear } from "../enums/enums.ts";
 
 const MiniProfile = (props) => {
   const onCancel = () => {
     props.onCancel(false);
   };
+  console.log(props.user.job_start_year);
   return (
     <div className="mini-profile">
       <div className="backdrop" onClick={onCancel} />
       <Card className="mini-profile-card">
         <header className="mini-profile-header">
           <Cancel className="mini-profile-cancel-btn" onClick={onCancel} />
-          <img src={props.user.user_image} alt="תמונה של המשתמש" />
+          <img
+            className="mini-profile-img"
+            src={props.user.user_image}
+            alt="תמונה של המשתמש"
+          />
           <h2> {props.user.private_name + " " + props.user.last_name}</h2>
         </header>
         <Separator />
         <div className="mini-profile-contents">
           <div className="mini-profile-content">
-            <div>{props.user.user_email}</div>
             <Mail className="mini-profile-icon" />
+            <div>{props.user.user_email}</div>
           </div>
           <div className="mini-profile-content">
-            <div>{props.user.birthday_date}</div>
             <Birthday className="mini-profile-icon" />
+            <div>{props.user.birthday_date}</div>
           </div>
           <div className="mini-profile-content">
-            <div>{props.user.faculty}</div>
             <College className="mini-profile-icon" />
+            <div>{getFaculty(props.user.faculty)}</div>
           </div>
           <div className="mini-profile-content">
-            <div>{props.user.year}</div>
             <Year className="mini-profile-icon" />
+            <div>{getYear(props.user.year)}</div>
           </div>
-          <div className="mini-profile-content">
-            <div>{props.user.job_company_name}</div>
-            <Job className="mini-profile-icon" />
-          </div>
+          {props.user.job_start_year !== 0 && (
+            <div className="mini-profile-content">
+              <Job className="mini-profile-icon" />
+              <div>
+                {props.user.job_company_name}, החל מ-{props.user.job_start_year}
+              </div>
+            </div>
+          )}
         </div>
       </Card>
     </div>
