e02a8b7df0e869b7b4a8cab709ee2bea434308f3
pazbechor <pbechor@paloaltonetworks.com>
2023-06-25 20:35:32 +0300

    Adding table models & rest models of forum Questions & comments

---
+++ BE/lib/utils/db/models/question.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import Column, String, Enum, DateTime
+from sqlalchemy.orm import relationship
+
+from ..user_db import Base
+from ...enums import Faculty
+
+
+class Question(Base):
+    __tablename__ = 'question'
+
+    question_id = Column(String, primary_key=True, index=True)
+    author = Column(String, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    faculty = Column(Enum(Faculty), nullable=True)
+    published_time = Column(DateTime, default=datetime.utcnow)
+    question_comments = relationship("QuestionComment", back_populates="question", lazy="joined")
---
+++ BE/lib/utils/db/models/question_comment.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import Column, String, Integer, ForeignKey, JSON, DateTime
+from sqlalchemy.orm import relationship
+
+from ..user_db import Base
+
+
+class QuestionComment(Base):
+    __tablename__ = "question_comment"
+
+    comment_id = Column(String, primary_key=True, unique=True, index=True)
+    author = Column(String, nullable=False)
+    content = Column(String)
+    published_time = Column(DateTime, default=datetime.utcnow)
+    likes = Column(JSON, nullable=True, default=[])  # Use ARRAY data type for likes
+
+    question_id = Column(String, ForeignKey('question.question_id'))
+    question = relationship("Question", back_populates="question_comments", lazy="joined")
+
+    def add_like(self, user_email) -> List[str]:
+        likes = self.likes.copy()
+        if user_email not in self.likes:
+            likes.append(user_email)
+        return likes
+
+    def remove_like(self, user_email: str) -> List[str]:
+        likes = self.likes.copy()
+        if user_email in self.likes:
+            likes = [email for email in likes if email != user_email]
+        return likes
